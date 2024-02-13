b6835f77769d2dfc969118afdb7b0c09cebabea4
[Samuel Iago] <samueliagocabral@gmail.com>
2024-02-13 14:33:06 -0300

    prototipo e bibliotecas

--- cliente.h
+++ cliente.h
@@ -3,4 +3,6 @@ typedef struct Clientes Cliente;
 
 
 /*Funcao que cadastra Cliente e retorna um vetor de estrutura Cliente*/
-Cliente*cadastrarCliente(Cliente ** cliente);
+Cliente*cadastrarCliente(Cliente* cliente);
+/*Funcao que recebe os dados do arquivo txt*/
+void receberDadosDoArquivo(Cliente ** cliente);
--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cliente.h"
 int main(){
+    FILE *Clientes = fopen("clientes.txt","at");
+    if(Clientes == NULL){
+        printf("Erro ao abrir o arquivo clientes.txt");
+        exit(1);
+    }
 
 
-
-
+    fclose(Clientes);
 return 0;
 }
