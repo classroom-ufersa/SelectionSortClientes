a5ba4b4f186f8877210f4ce7cb8eb4922ddd4ace
[Samuel Iago] <samueliagocabral@gmail.com>
2024-02-13 15:40:47 -0300

    funcao selectionSort

--- cliente.c
+++ cliente.c
@@ -10,30 +10,56 @@ struct Clientes
     int codigoCliente;
 };
 
-Cliente *cadastrarCliente(Cliente* cliente){
-    cliente = (Cliente*) malloc (sizeof(Cliente));
+Cliente *cadastrarCliente(Cliente *cliente)
+{
+    cliente = (Cliente *)malloc(sizeof(Cliente));
     printf("Insira o seu nome:\n");
-    scanf(" %[^\n]s",cliente->nome);
+    scanf(" %[^\n]s", cliente->nome);
     printf("Insira o seu endereco:\n");
-    scanf(" %[^\n]s",cliente->endereco);
+    scanf(" %[^\n]s", cliente->endereco);
     printf("Insira o seu codigo:\n");
-    scanf("%d",&cliente->codigoCliente);
+    scanf("%d", &cliente->codigoCliente);
     return cliente;
 }
 
-void receberDadosDoArquivo(Cliente ** cliente,FILE*arquivo){
-// carregar dados do arquivo
-    int contadorClientes=0;
+void receberDadosDoArquivo(Cliente **cliente, FILE *arquivo, int *contadorClientes)
+{
+    // carregar dados do arquivo
     char linha[100];
-    for(contadorClientes=0;fgets(linha,sizeof(linha),arquivo)!=NULL;contadorClientes++){
-        cliente=(Cliente**)realloc(cliente,(contadorClientes+1)*sizeof(Cliente*));
-        cliente[contadorClientes]=(Cliente*)malloc((contadorClientes+1)*sizeof(Cliente));
-        fscanf(arquivo," %s,%s,%d\n", cliente[contadorClientes]->nome, cliente[contadorClientes]->endereco, &cliente[contadorClientes]->codigoCliente);
+    for (*contadorClientes = 0; fgets(linha, sizeof(linha), arquivo) != NULL; *contadorClientes++)
+    {
+        cliente = (Cliente **)realloc(cliente, (*contadorClientes + 1) * sizeof(Cliente *));
+        cliente[(*contadorClientes)] = (Cliente *)malloc((*contadorClientes + 1) * sizeof(Cliente));
+        fscanf(arquivo, " %[^,],%[^,],%d\n", cliente[(*contadorClientes)]->nome, cliente[(*contadorClientes)]->endereco, &cliente[(*contadorClientes)]->codigoCliente);
     }
-// contadorClientes contem o numero de linhas que estavam preenchidas/total de clientes -> serve para (free) e para o (for) de (cadastrar cliente) 
+    // contadorClientes contem o numero de linhas que estavam preenchidas/total de clientes -> serve para (free) e para o (for) de (cadastrar cliente)
 }
 
 
+
+void selectionSort(Cliente **clientes, int *contadorClientes)
+{
+    char nomeTemporario[100];
+    int i = 0, j = 0;
+    int primeiro = 0;
+    for (i = 0; i < contadorClientes + 1; i++)
+    {
+        primeiro = i;
+        for (j = i + 1; j < contadorClientes; j++)
+        {
+            if (strcmp(clientes[j]->nome, clientes[primeiro]->nome) < 0)
+            {
+                primeiro = j;
+            }
+            if (primeiro != i)
+            {
+                strcpy(nomeTemporario, clientes[i]->nome);
+                strcpy(clientes[i]->nome, clientes[j]->nome);
+                strcpy(clientes[j]->nome, nomeTemporario);
+            }
+        }
+    }
+}
 // quando for receber o novo cliente precisa reallocar contador de clientes + 1 para poder criar um espeço para vetor novo
 // cliente=(Cliente**)realloc(cliente,(contadorClientes+1)*sizeof(Cliente*));
 // cliente[contadorClientes+1]=cadastrarCliente(cliente[contadorClientes+1]);
