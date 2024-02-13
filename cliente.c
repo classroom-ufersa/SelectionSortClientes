6dd91ceb62fbf420da8ae2bfc1207f92c459b9d4
[Samuel Iago] <samueliagocabral@gmail.com>
2024-02-13 13:38:26 -0300

    funcao cadastrarCliente

--- cliente.c
+++ cliente.c
@@ -1,7 +1,22 @@
 #include "cliente.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct Clientes{
+struct Clientes
+{
     char nome[50];
     char endereco[50];
     int codigoCliente;
 };
+
+Cliente *cadastrarCliente(Cliente* cliente){
+    cliente = (Cliente*) malloc (sizeof(Cliente));
+    printf("Insira o seu nome:\n");
+    scanf(" %[^\n]s",cliente->nome);
+    printf("Insira o seu endereco:\n");
+    scanf(" %[^\n]s",cliente->endereco);
+    printf("Insira o seu codigo:\n");
+    scanf("%d",&cliente->codigoCliente);
+    return cliente;
+}
