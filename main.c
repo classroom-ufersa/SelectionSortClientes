9a297628554d0ea0940796e292bbfc07714acef3
[Samuel Iago] <samueliagocabral@gmail.com>
2024-02-20 09:44:24 -0300

    a+

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "cliente.h"
 int main(){
-    FILE *arquivo = fopen("clientes.txt","at");
+    FILE *arquivo = fopen("clientes.txt","a+");
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo clientes.txt");
         exit(1);
