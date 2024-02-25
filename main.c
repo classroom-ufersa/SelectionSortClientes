becbdccda273f5a67532623f572b71456f3962d8
Leandro Carlos <147527757+LeandroCarl@users.noreply.github.com>
2024-02-25 14:49:05 +0000

    Implementação da função principal na main

--- main.c
+++ main.c
@@ -2,16 +2,8 @@
 #include <stdlib.h>
 #include "cliente.h"
 int main(){
-    FILE *arquivo = fopen("clientes.txt","a+");
-    if(arquivo == NULL){
-        printf("Erro ao abrir o arquivo clientes.txt");
-        exit(1);
-    }
-    Cliente**cliente = NULL;
-    int contadorCliente=0;
-    int *pontContadorCliente=&contadorCliente;
-    receberDadosDoArquivo(cliente, arquivo, pontContadorCliente);
-    liberarMemoria(cliente, *pontContadorCliente);
-    fclose(arquivo);
-return 0;
+    
+    funcaoPrincipal();
+
+    return 0;
 }
